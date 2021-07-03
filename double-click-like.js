91f54957c92d7fdd9a7d0e52a6cc2144e1031c78
Anh Vo <84301202+votqanh@users.noreply.github.com>
2021-07-03 12:37:23 +0630

    Fix bug in notification comments

--- double-click-like.js
+++ double-click-like.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Double Click to Like YouTube Comments
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Double click on a comment to like. Double click again to remove like.
 // @author       votqanh
 // @match        *://*.youtube.com/*
+// @icon         https://t4.ftcdn.net/jpg/02/64/18/21/360_F_264182169_79nqopuqFnMqbeLw3c7u0CiFW5Cwj1ah.jpg
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require      https://greasyfork.org/scripts/5392-waitforkeyelements/code/WaitForKeyElements.js?version=19641
 // @grant        GM_addStyle
@@ -14,37 +15,35 @@
     'use strict';
 
     window.addEventListener('load', function() {
-        var cmtSection = "#sections";
-        var notifCmt = "ytd-comments.ytd-multi-page-menu-renderer"
+        var videoBox = "#sections";
+        var notifBox = "ytd-comments.ytd-multi-page-menu-renderer"
 
         function removeHighlight(el) {
             waitForKeyElements(el, function(e) {
                 document.querySelector(el).addEventListener('mousedown', function(e) {
-                    if (e.detail > 1 && e.target.id != "contenteditable-root") e.preventDefault()
+                    if (e.detail > 1 && e.target.id != "contenteditable-root") {
+                        e.preventDefault();
+                    }
                 });
             });
         }
 
-        removeHighlight(cmtSection);
-        removeHighlight(notifCmt);
+        removeHighlight(videoBox);
+        removeHighlight(notifBox);
 
 
         var cmt = "ytd-comment-renderer#comment";
         var reply = "[is-reply]"
 
         function waitFor(el) {
-            var cnt = 0;
-
-            function addDbl(e) {
-                if (e.target.id != "contenteditable-root") event.target.closest(el).querySelector("#like-button").click()
+            function like(e) {
+                if (e.target.id != "contenteditable-root") {
+                    e.target.closest(el).querySelector("#like-button").click();
+                }
             }
 
-            waitForKeyElements(el, function() {
-                var item = document.querySelectorAll(el)[cnt];
-
-                item.addEventListener('dblclick', addDbl);
-
-                cnt++;
+            waitForKeyElements(el, function (jNode) {
+                jNode[0].addEventListener('dblclick', like);
             });
         }
 
